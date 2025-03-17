f355790953bfbd4b9b05bd049081cea623299096
javadvhd <vahedi.r46@gmail.com>
2025-03-17 12:17:19 +0330

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,258 @@
+import { ThemeProvider } from "setup/theme/theme-provider";
+import { Profile } from "components/profile/profile";
+import "./App.css";
+import { Chip, Grid2, Grid2Props, styled } from "@mui/material";
+import { Typography } from "components/ui/typography";
+import {
+  ResumeCard,
+  ResumeCardColor,
+  ResumeCardItem,
+} from "components/ui/resume-card";
+
+const SPACING = 4;
+function App() {
+  return (
+    <>
+      <ThemeProvider>
+        <Root>
+          <Profile sx={{ mb: SPACING }} />
+          <Grid2 container spacing={SPACING}>
+            <Column>
+              <ResumeCard
+                radius={{ topLeft: true, bottomRight: true }}
+                title="Experience"
+                color={ResumeCardColor.LILAC}
+              >
+                <ResumeCardItem>
+                  <ExperienceItem>
+                    <Typography component="pre">
+                      {"Jan, 2021 - present \n Weblite"}
+                    </Typography>
+                    <ExperienceItemDesc>Front end developer</ExperienceItemDesc>
+                  </ExperienceItem>
+                </ResumeCardItem>
+              </ResumeCard>
+            </Column>
+            <Column>
+              <ResumeCard
+                radius={{ bottomRight: true }}
+                sx={{ backgroundColor: "#fcede1" }}
+                title="Skills"
+                color={ResumeCardColor.CREAM}
+              >
+                <ResumeCardItem
+                  sx={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    gap: 1,
+                  }}
+                >
+                  <SkillsChips
+                    skills={[
+                      "Typescript",
+                      "WebSocket",
+                      "ReactJS",
+                      "Mui-Material",
+                      "NextJS",
+                      "CSS/SCSS",
+                    ]}
+                  />
+                </ResumeCardItem>
+              </ResumeCard>
+            </Column>
+
+            <Column size={{ xs: 12, sm: 12 }}>
+              <ResumeCard
+                radius={{
+                  topRight: true,
+                  bottomLeft: true,
+                }}
+                color={ResumeCardColor.LILAC}
+                title="Projects"
+              >
+                {projects.map((project) => (
+                  <ProjectsResumeCardContent
+                    {...project}
+                    key={
+                      typeof project.name === "string"
+                        ? project.name
+                        : project.name.value
+                    }
+                  />
+                ))}
+              </ResumeCard>
+            </Column>
+
+            <Grid2 container size={{ xs: 12 }} rowGap={SPACING}>
+              <ResumeCard
+                color={ResumeCardColor.CREAM}
+                radius={{ topLeft: true }}
+                title="Education"
+              >
+                <ResumeCardItem>
+                  <Typography>2017 - 2021</Typography>
+                  <Typography fontWeight="bold">
+                    Bachelor's Degree JAVAD_MAJOR
+                  </Typography>
+                  <Typography>Tehran University</Typography>
+                </ResumeCardItem>
+              </ResumeCard>
+
+              <ResumeCard
+                title="Language"
+                color={ResumeCardColor.LILAC}
+                radius={{ bottomRight: true }}
+              >
+                <ResumeCardItem>
+                  <Typography>English</Typography>
+                  <Typography>Persian</Typography>
+                </ResumeCardItem>
+              </ResumeCard>
+
+              <ResumeCard
+                color={ResumeCardColor.LILAC}
+                radius={{ bottomLeft: true }}
+                title="Contact"
+              >
+                <ResumeCardItem>
+                  <Typography fontWeight="bold">Phone</Typography>
+                  <Typography>JAVAD_PHONE</Typography>
+                </ResumeCardItem>
+                <ResumeCardItem>
+                  <Typography fontWeight="bold">Email</Typography>
+                  <Typography>JAVAD_EMAIL</Typography>
+                </ResumeCardItem>
+                <ResumeCardItem>
+                  <Typography fontWeight="bold">Address</Typography>
+                  <Typography>Tehran, Iran</Typography>
+                </ResumeCardItem>
+              </ResumeCard>
+            </Grid2>
+          </Grid2>
+        </Root>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
+
+const Root = styled("div")`
+  padding: ${({ theme }) => theme.spacing(2)};
+`;
+
+const Column = (props: Grid2Props) => {
+  return (
+    <Grid2
+      size={{ xs: 12, sm: 6 }}
+      container
+      alignItems="center"
+      direction="column"
+      rowGap={SPACING}
+      {...props}
+    />
+  );
+};
+
+const ExperienceItem = (props: Grid2Props) => {
+  return (
+    <Grid2
+      flexWrap="wrap"
+      container
+      columnGap={2}
+      alignItems="flex-start"
+      {...props}
+    />
+  );
+};
+
+const ExperienceItemDesc = styled(Typography)`
+  ${({ theme }) => theme.breakpoints.between("sm", "md")} {
+    max-width: 110px;
+  }
+  font-weight: bold;
+`;
+
+const SkillsChips = ({ skills }: { skills: string[] }) => {
+  return (
+    <>
+      {skills.map((skill) => (
+        <Chip key={skill} label={skill} variant="outlined" />
+      ))}
+    </>
+  );
+};
+
+interface Project {
+  name: string | { href: string; value: string };
+  company: string | { href: string; value: string };
+  desc?: string;
+  image?: {
+    src: string;
+    width: number;
+    height: number;
+  };
+}
+const projects: Project[] = [
+  {
+    name: { value: "Weblite - ReactJs", href: "https://weblite.me" },
+    company: "Tosee Ertebatate Asre Rosha",
+    desc: "A specialized platform for conducting educational courses on a messaging platform.",
+  },
+  {
+    name: "Comunication Services",
+    company: "Tosee Ertebatate Asre Rosha",
+    desc: "Platforms designed for one-on-one calls, group meetings, live classes, webinars, and more. These apps offer advanced features such as video sharing, screen sharing, audio streaming, and file sharing to enhance collaboration and interaction.",
+  },
+  {
+    name: "General exam",
+    company: "Tosee Ertebatate Asre Rosha",
+    desc: "Conducting multiple-choice and descriptive exams. Viewing the answer sheet after completing the exam and automatically calculating students' scores.",
+    image: {
+      src: "/images/general-exam.jpeg",
+      width: 146,
+      height: 260,
+    },
+  },
+];
+const ProjectsResumeCardContent = ({ name, company, desc, image }: Project) => {
+  return (
+    <ResumeCardItem>
+      <Typography
+        fontWeight="bold"
+        href={typeof name === "string" ? undefined : name.href}
+      >
+        {typeof name === "string" ? name : name.value}
+      </Typography>
+      <Typography
+        mb={desc ? 0.5 : 0}
+        variant="body2"
+        href={typeof company === "string" ? undefined : company.href}
+      >
+        {typeof company === "string" ? company : company.value}
+      </Typography>
+      {image && (
+        <ProjectImage
+          src={image.src}
+          alt={typeof name === "string" ? name : name.value}
+          sx={{
+            width: `${image.width}px`,
+            height: `${image.height}px`,
+          }}
+        />
+      )}
+      {desc && <Typography>{desc}</Typography>}
+    </ResumeCardItem>
+  );
+};
+
+const ProjectImage = styled("img")`
+  margin-left: 30px;
+  border-radius: 8px;
+  margin-top: 4px;
+  margin-bottom: 8px;
+  @media (max-width: 768px) {
+    align-self: center;
+    margin-left: 0;
+  }
+`;
---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,53 @@
+import { Grid2, styled, SxProps, Typography } from "@mui/material";
+import { ResumeCard } from "components/ui/resume-card";
+import { WithBorderBottom } from "components/ui/with-border-bottom";
+
+interface Props {
+  sx?: SxProps;
+  className?: string;
+}
+export function Profile({ sx, className }: Props) {
+  return (
+    <Grid2 sx={sx} className={className} container alignItems="center">
+      <Grid2 size={{ xs: 12, sm: 6 }} container justifyContent="center">
+        <ProfileImage radius={{ topRight: true, bottomLeft: true }} />
+      </Grid2>
+      <Grid2 size={{ xs: 12, sm: 6 }} sx={{ px: 4 }} alignItems="flex-start">
+        <Typography variant="h3" textAlign="left">
+          Javad Vahedi
+        </Typography>
+        <Typography variant="subtitle1" textAlign="left" mb={3}>
+          Software Developer
+        </Typography>
+
+        <WithBorderBottom sx={{ mb: 1.5 }}>
+          <Typography variant="h4" textAlign="left">
+            Profile
+          </Typography>
+        </WithBorderBottom>
+
+        <Typography variant="body1" textAlign="left">
+          Hello! I am Javad Vahedi, a skilled software developer with six years of experience specializing in React and TypeScript. Proven expertise in addressing complex challenges, demonstrated through work on a large-scale messenger project involving communication services (meet, call, classroom, live), stories, chat, WebSocket, IndexedDB, and Redux. Known for a detail-oriented and collaborative approach, delivering accurate solutions efficiently while excelling in problem-solving and adaptability.
+        </Typography>
+      </Grid2>
+    </Grid2>
+  );
+}
+
+const ProfileImage = styled(ResumeCard)`
+  height: 230px;
+  width: 100%;
+  max-width: 140px;
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
+  ${({ theme }) => theme.breakpoints.up("sm")} {
+    height: 400px;
+    width: 100%;
+    max-width: 250px;
+    margin-bottom: 0;
+  }
+  background-image: url("./images/profile-small.jpg");
+  background-position: center;
+  background-color: #fcede180;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
